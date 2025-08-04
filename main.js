40b2ba4111a0d8eaf834770792076fcaa5995d4a
CarlosV <carlos.viller98@gmail.com>
2025-08-04 19:34:06 -0300

    auth init

--- main.js
+++ main.js
@@ -1,7 +1,41 @@
-import Afip from '@afipsdk/afip.js';
+import fs from "fs";
+import Afip from "@afipsdk/afip.js";
+import { config } from "process";
 
-const afip = new Afip({ CUIT: 20409378472 });
+const CUIT = 20956569592;
+const PRODUCTION = false;
 
-const voucherTypes = await afip.ElectronicBilling.getVoucherTypes();
+function getAuth() {
+  const PATH = PRODUCTION ? "prod/" : "dev/";
+  // Certificado (Puede estar guardado en archivos, DB, etc)
+  const cert = fs.readFileSync(`${PATH}cert.crt`, { encoding: "utf8" });
 
-console.log(voucherTypes);
+  // Key (Puede estar guardado en archivos, DB, etc)
+  const key = fs.readFileSync(`${AUTH}afip.key`, { encoding: "utf8" });
+
+  return { cert, key };
+}
+
+const config = {
+  CUIT,
+  ...getAuth(),
+};
+
+const afip = new Afip(config);
+const ws = afip.WebService("wsfex");
+
+// Obtenemos el TA
+const ta = await ws.getTokenAuthorization();
+
+// Preparamos los datos
+const data = {
+  Auth: {
+    Token: ta.token,
+    Sign: ta.sign,
+    Cuit: afip.CUIT,
+  },
+};
+
+const res = await ws.executeRequest("FEXGetLast_ID", data);
+
+console.log(res);
