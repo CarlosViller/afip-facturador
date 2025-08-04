00f26ff16902f5a155d6f52d3c052aa01915f9fe
CarlosV <carlos.viller98@gmail.com>
2025-08-04 19:56:48 -0300

    prod

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 import fs from "fs";
 import Afip from "@afipsdk/afip.js";
-import { config } from "process";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const CUIT = 20956569592;
-const PRODUCTION = false;
+const PRODUCTION = true;
 
 function getAuth() {
   const PATH = PRODUCTION ? "prod/" : "dev/";
   // Certificado (Puede estar guardado en archivos, DB, etc)
-  const cert = fs.readFileSync(`${PATH}cert.crt`, { encoding: "utf8" });
+  const cert = fs.readFileSync(`./${PATH}cert.crt`, { encoding: "utf8" });
 
   // Key (Puede estar guardado en archivos, DB, etc)
-  const key = fs.readFileSync(`${AUTH}afip.key`, { encoding: "utf8" });
+  const key = fs.readFileSync(`./${PATH}key.key`, { encoding: "utf8" });
 
   return { cert, key };
 }
@@ -19,23 +21,33 @@ function getAuth() {
 const config = {
   CUIT,
   ...getAuth(),
+  access_token: process.env.ACCESS_TOKEN,
+  production: PRODUCTION,
 };
 
 const afip = new Afip(config);
-const ws = afip.WebService("wsfex");
 
-// Obtenemos el TA
-const ta = await ws.getTokenAuthorization();
+// Periodo del CAEA. (yyyymm)
+const periodo = 202506;
 
-// Preparamos los datos
-const data = {
-  Auth: {
-    Token: ta.token,
-    Sign: ta.sign,
-    Cuit: afip.CUIT,
-  },
-};
+// Orden del CAEA dentro del periodo. Quincena 1, Quincena 2
+const orden = 1;
 
-const res = await ws.executeRequest("FEXGetLast_ID", data);
+const caeaInfo = await afip.ElectronicBilling.getCAEA(periodo, orden);
+// const ws = afip.WebService("wsfex");
 
-console.log(res);
+// // Obtenemos el TA
+// const ta = await ws.getTokenAuthorization();
+
+// // Preparamos los datos
+// const data = {
+//   Auth: {
+//     Token: ta.token,
+//     Sign: ta.sign,
+//     Cuit: afip.CUIT,
+//   },
+// };
+
+// const res = await ws.executeRequest("FEXGetLast_ID", data);
+
+console.log(caeaInfo);
